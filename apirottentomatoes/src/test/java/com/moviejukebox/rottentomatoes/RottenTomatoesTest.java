e82bd60b6661332979db61abd1dccf2531abfddf
Omertron <Stuart.Boston@Gmail.com>
2011-10-27 17:27:50 +0000

    Updated tests

--- apirottentomatoes/src/test/java/com/moviejukebox/rottentomatoes/RottenTomatoesTest.java
+++ apirottentomatoes/src/test/java/com/moviejukebox/rottentomatoes/RottenTomatoesTest.java
@@ -16,79 +16,86 @@ import java.util.Set;
 
 import junit.framework.TestCase;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import com.moviejukebox.rottentomatoes.model.Cast;
 import com.moviejukebox.rottentomatoes.model.Link;
 import com.moviejukebox.rottentomatoes.model.Movie;
 import com.moviejukebox.rottentomatoes.model.Review;
 
-public class Test extends TestCase {
-    public RottenTomatoes rt = new RottenTomatoes("rnt8xak564a8sxkts5xkqj5z");
+public class RottenTomatoesTest extends TestCase {
+    public RottenTomatoes rt;
 
-    public Test(String name) {
-        super(name);
-    }
-    
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
+        rt = new RottenTomatoes("rnt8xak564a8sxkts5xkqj5z");
     }
 
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
+    @Test
     public void testGetLists() {
         Set<Link> response = rt.getLists();
         assertEquals(2, response.size());
     }
     
+    @Test
     public void testMoviesSearch() {
         Set<Movie> response = rt.moviesSearch("Blade Runner");
-        assertEquals(3, response.size());
+        assertTrue(response.size() > 0);
     }
 
-    public void NOtestListsDirectory() {
+    @Test
+    public void testListsDirectory() {
         Set<Link> response = rt.listsDirectory();
         assertEquals(2, response.size());
     }
     
+    @Test
     public void testMovieListsDirectory() {
         Set<Link> response = rt.movieListsDirectory();
         assertEquals(4, response.size());
     }
     
+    @Test
     public void testDvdListsDirectory() {
         Set<Link> response = rt.dvdListsDirectory();
         assertTrue(response.size() > 0);
     }
 
+    @Test
     public void testOpeningMovies() {
         int maxMovies = 10;
         Set<Movie> response = rt.openingMovies(maxMovies);
         assertTrue(response.size() <= maxMovies);
     }
 
+    @Test
     public void testUpcomingMovies() {
         int maxMovies = 10;
         Set<Movie> response = rt.upcomingMovies(maxMovies);
         assertTrue((response.size() > 0) && (response.size() <= maxMovies));
     }
 
+    @Test
     public void testNewReleaseDvds() {
         int maxMovies = 10;
         Set<Movie> response = rt.newReleaseDvds(maxMovies);
         assertTrue((response.size() > 0) && (response.size() <= maxMovies));
     }
 
+    @Test
     public void testMovieInfo() {
         Movie movie = rt.movieInfo(12886);
         assertEquals("Blade Runner", movie.getTitle());
     }
     
+    @Test
     public void testMovieCast() {
         Set<Cast> castList = rt.movieCast(12886);
         assertTrue(castList.size() > 0);
     }
 
+    @Test
     public void testMovieReviews() {
         Set<Review> reviewList = rt.movieReviews(12886, Review.ReviewType.all);
         assertTrue(reviewList.size() > 0);
