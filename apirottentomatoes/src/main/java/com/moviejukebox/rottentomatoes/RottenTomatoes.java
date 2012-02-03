a43cf63225cecae5877bd971f952bdad05684531
Omertron <Stuart.Boston@Gmail.com>
2012-02-03 15:57:47 +0000

    Updated commons lang version

--- apirottentomatoes/src/main/java/com/moviejukebox/rottentomatoes/RottenTomatoes.java
+++ apirottentomatoes/src/main/java/com/moviejukebox/rottentomatoes/RottenTomatoes.java
@@ -25,7 +25,7 @@ import java.net.URLEncoder;
 import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Set;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
